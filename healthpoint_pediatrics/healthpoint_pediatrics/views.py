abecc5684c570b5f1fc6a8bbe8abda5d3b5be132
mplot0139 <mplot0139@gmail.com>
2015-11-08 17:27:00 +0000

    Update url.py and view.py

--- healthpoint_pediatrics/healthpoint_pediatrics/views.py
+++ healthpoint_pediatrics/healthpoint_pediatrics/views.py
@@ -1,5 +1,10 @@
 from django.http import Http404, HttpResponse
 
 def main_view(request, page):
-    page = page[:-1]
+    page = page[:-1]                        # remove trailing forward slash
+    links = activate_link(page)             # set "active" link
     return HttpResponse('<h1>' + page + '</h1>')
+
+# Takes the page name and adds "active" class to corresponding link    
+def activate_link(page):
+    return
