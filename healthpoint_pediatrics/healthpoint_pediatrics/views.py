a51c93ffe47e8b398f5d8ba527ed1a37c78701d1
mplot0139 <mplot0139@gmail.com>
2015-11-09 02:26:01 +0000

    Fix views and templates to update links and title

--- healthpoint_pediatrics/healthpoint_pediatrics/views.py
+++ healthpoint_pediatrics/healthpoint_pediatrics/views.py
@@ -2,20 +2,29 @@ import os
 import html
 from django.http import Http404
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
 from .settings import BASE_DIR
 
 def main_view(request, page):
     if not page:                            # set domain level access to 'home'
         page = 'home/'
     page = page[:-1]                        # remove trailing forward slash
-    links = activate_link(page)             # set "active" link
-    context = {'title': page,
-                'main_menu': links}
-    return render(request, 'base.html', context)
-
-# Takes the page name and adds "active" class to corresponding link    
-def activate_link(page):
-    main_menu = os.path.join(BASE_DIR, 'templates/main_menu.html')
-    with open(main_menu) as f:
-        links = f.read()
-    return links
+    context = {
+        'title': page,
+        'menu': {
+            'home': '',
+            'about': '',
+            'providers': '',
+            'services': '',
+            'location': '',
+            'hours': '',
+            'financial': '',
+            'contact': ''
+            }
+        }
+        
+    for key in context['menu']:
+        if key == page:
+            context['menu'][key] = mark_safe('class="active"')
+            
+    return render(request, 'main.html', context)
