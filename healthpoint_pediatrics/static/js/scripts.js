00b1adccb550d9173e9602f3dc60230bab7018d2
mplot0139 <mplot0139@gmail.com>
2015-11-15 23:00:52 +0000

    refactoring

--- healthpoint_pediatrics/static/js/scripts.js
+++ healthpoint_pediatrics/static/js/scripts.js
@@ -4,47 +4,27 @@ $(document).ready(function() {
 *       INITIAL SETUP
 ******************************************************************/
 
-    // Set correct padding on initial loading
-    setPadding();
-    
     // get visible section, active menu, and .content objects
-    var currentVisibleSection = $("#content section").not(".hidden");
-    var currentActiveMenu = $("#main-nav.navbar ul li").filter(".active");
-    var $content = $("#content");
+    var $currentVisibleSection = $("#content section").not(".hidden");
+    var $currentActiveMenu = $("#main-nav.navbar ul li").filter(".active");
+    var $content = $('#content');
     var currentBgClass = $content.attr('class');
+    
+    // Set correct top padding on initial loading
+    setTopPadding();
+    setBottomPadding();
 
-   // set up event handlers for menu buttons clicks
-    // menu link onclick event handler
-    $(".navbar ul li").on("click", function(e){
-        var newActiveMenu = e.delegateTarget;
-        var newSectionId = $(e.target).attr("href").slice(1);
-        var newVisibleSection = $("#" + newSectionId);
-
-        // change active link
-        $(newActiveMenu).addClass("active");
-        $(currentActiveMenu).removeClass("active");
-        currentActiveMenu = newActiveMenu;
-        
-        // change site title
-        var newTitle = newSectionId.charAt(0).toUpperCase() + newSectionId.slice(1);
-        $("title").text(newTitle);
-        
-        // hide current visible section
-        $(currentVisibleSection).addClass("hidden");
-        
-        // change background with fading out/in animation
-        var newBgClass = "bg-" + newSectionId;
-        $content.animate({opacity: 0}, 'slow', function() {
-            $("#content").removeClass(currentBgClass);
-            currentBgClass = newBgClass;
-            $("#content").addClass("bg-" + newSectionId).animate({opacity: 1}, 'slow', function() {
-                // show new visible section
-                $(newVisibleSection).removeClass("hidden");
-                currentVisibleSection = newVisibleSection;
-            });
-        });
-            
+/***********************************************
+ *      EVENT HANDLERS
+ * ********************************************/
+ 
+    // Adjust padding on resize
+    $(window).resize(function() {
+        // correct padding
+        setTopPadding();
+        setBottomPadding();
     });
+    
     // collapses the menu on a xs screen after the link was clicked
     $(document).on('click', 'nav .navbar-collapse.in', function(e) {
         if ( $(e.target).is('a') ) {
@@ -52,26 +32,67 @@ $(document).ready(function() {
         }
     });
     
-/***********************************************
- *      EVENT HANDLERS
- * ********************************************/
- 
- // Adjust padding on resize
-    $(window).resize(function() {
-        setPadding();
+    // menu link onclick event handler
+    $(".navbar ul li").on("click", function(e){
+        var $newActiveMenu = $(e.delegateTarget);
+        var newSectionId = $(e.target).attr("href").slice(1);
+        var $newVisibleSection = $("#" + newSectionId);
+        var newBgClass = "bg-" + newSectionId;
+
+        // change active link
+        changeActiveLink($newActiveMenu);
+        
+        // change site title
+        changeSectionTitle (newSectionId);
+        
+        // change background with fading /in animation
+        chgBgAndSectionAnimation (newBgClass, newSectionId, $newVisibleSection);
     });
+    
     
 /***********************************************
  *      HELPER FUNCTIONS
  * ********************************************/
  
-  /** Sets padding on the #content based on navbar height
+    /** Sets padding on the #content based on navbar height
      * On smaller screens navbar displays in two lines and doubles
      * in size. The function sets body padding-top equal to the 
      * navbar height
      **/
-    function setPadding() {
+    function setTopPadding() {
         var padding = $('#main-nav.navbar div.container').css('height');
-        $('#content').css('padding-top', padding );
+        $content.css('padding-top', padding );
+    }
+    
+    function setBottomPadding() {
+        var padding = $('footer').css('height');
+        $('#content section').css('padding-bottom', padding);
+    }
+    
+    function changeActiveLink($newActiveMenu) {
+        $newActiveMenu.addClass("active");
+        $currentActiveMenu.removeClass("active");
+        $currentActiveMenu = $newActiveMenu;
+    }
+    
+    function changeSectionTitle (newSectionId) {
+        var newTitle = newSectionId.charAt(0).toUpperCase() + newSectionId.slice(1);
+        $("title").text(newTitle);
+    }
+    
+    function chgBgAndSectionAnimation (newBgClass, newSectionId, $newVisibleSection) {
+        // hide current visible section
+        $currentVisibleSection.addClass("hidden");
+        $content.animate({opacity: 0}, 'slow', 
+            function() {
+                $content.removeClass(currentBgClass);
+                currentBgClass = newBgClass;
+                $content.addClass("bg-" + newSectionId).animate({opacity: 1}, 'slow', 
+                    function() {
+                        // show new visible section
+                        $newVisibleSection.removeClass("hidden");
+                        $currentVisibleSection = $newVisibleSection;
+                    });
+            });
     }
 });
